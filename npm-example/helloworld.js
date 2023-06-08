28002711252c44ee9ed90758a7ffc4c077ecca2a
Nandini Das <86686671+NandiniDJFrog@users.noreply.github.com>
2023-06-07 19:40:27 -0700

    Update helloworld.js

--- npm-example/helloworld.js
+++ npm-example/helloworld.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var electron = require('electron')
 
+console.log(electron)
+
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type' : 'text/plain'});
   res.end('Hello World\n');
